9dce64a8cb828ec3b1e1d283622ece7d26d20723
Sefa Y. Baach-Acheamphour <sacheamphour@Aluminium.NEWMARWINDOWS.local>
2022-03-28 11:33:26 -0400

    created ptoduct cards and display it

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.scss';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { UserProvider } from './contexts/user.context';
+import {ProductProvider} from './contexts/product.contest';
 
 const rootElement = document.getElementById('root');
 
@@ -11,7 +12,9 @@ render(
   <React.StrictMode>
     <BrowserRouter>
     <UserProvider>
+      <ProductProvider>
       <App />
+      </ProductProvider>
      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
